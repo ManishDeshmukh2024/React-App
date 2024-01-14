018b5345208c34b8663b3e3b10f3888a77596e4c
Manish Deshmukh <manish.2.deshmukh@gmail.com>
2024-01-14 11:45:46 -0500

    Functional Componebt and component composition

--- App.js
+++ App.js
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const displayJSXPower = <h1 id="displayJSX" className="JSXConversion">HELLO from JSX </h1>
+//Make functional Compenents in React
+// Functional Compenents are basically returning an JSX or React Elements.
+const HeaderComponent1 = () => {
+  return <h1 className="h1Header">Returning JSX from compondsfdsents</h1>;
+};
 
-console.log(displayJSXPower);
+const HeaderComponent = () => {
+  return (
+    <div>
+      <HeaderComponent1 />
+      <h1 className="h1Header">Returning JSX from components</h1>
+    </div>
+  );
+};
 
+console.log(HeaderComponent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(displayJSXPower);
+root.render(<HeaderComponent />);
